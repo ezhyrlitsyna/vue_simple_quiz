346c2999e70111772f2dcf8909fc0223225070f0
Eleonora <ezhyrlitsyna@gmail.com>
2023-04-04 14:21:21 +0300

    finish functionality

--- quiz/src/components/Questions.vue
+++ quiz/src/components/Questions.vue
@@ -1,27 +1,56 @@
 <template>
-<div class="questions-ctr">
-        <div class="progress">
-            <div class="bar"></div>
-            <div class="status">1 out of 3 questions answered</div>
-        </div>
-        <div class="single-question">
-            <div class="question">Sample Question 1</div>
-            <div class="answers">
-                <div class="answer">Sample Answer 1</div>
-                <div class="answer">Sample Answer 2</div>
-                <div class="answer">Sample Answer 3</div>
-                <div class="answer">Sample Answer 4</div>
-            </div>
-        </div>
+  <div class="questions-ctr">
+    <div class="progress">
+      <div
+        class="bar"
+        :style="{ width: `${(questionsAnswers / questions.length) * 100}%` }"
+      ></div>
+      <div class="status">
+        {{ questionsAnswers }} out of {{ questions.length }} questions answered
+      </div>
     </div>
+    <transition-group name="fade">
+      <div
+        class="single-question"
+        v-for="(question, index) in questions"
+        :key="question.q"
+        v-show="index === questionsAnswers"
+      >
+        <div class="question">{{ question.q }}</div>
+        <div class="answers">
+          <div
+            class="answer"
+            v-for="answer in question.answers"
+            :key="answer.text"
+            @click.prevent="selectAnswer(answer.is_correct)"
+          >
+            {{ answer.text }}
+          </div>
+        </div>
+      </div>
+    </transition-group>
+  </div>
 </template>
 
 <script>
-	export default {
-		
-	}
+export default {
+  props: {
+    questions: {
+      type: Array,
+      default: () => [],
+    },
+    questionsAnswers: {
+      type: Number,
+      default: null,
+    },
+  },
+  emits: { question_answered: null },
+  methods: {
+    selectAnswer(is_correct) {
+      this.$emit('question_answered', is_correct);
+    },
+  },
+};
 </script>
 
-<style>
-
-</style>
+<style></style>
